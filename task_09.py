d7e629353ede4778cb656a8978eb68ce0898b641
Amir Abdullaev <abdullaev.amir@gmail.com>
2016-09-11 09:33:48 +0000

    Tasks from 6 to 10 complete

--- task_09.py
+++ task_09.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question.
+
+This is new paragraph to a docstring
+This is new line to a new paragraph
+"""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
